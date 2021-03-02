61ffd9e9679da77e6349c576c68a35ea40319b95
unknown <luizpaulo.redes@gmail.com>
2021-03-02 11:47:16 -0300

    second-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  listTitles = [{
+    trailer:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover:'assets/images/guardios-da-galaxia.jpg',
+    name:'Guardiões da Galaxia',
+    relevance:98,
+    age:12,
+    parts:2,
+    categories:['Épico', 'Filme de fantasia','Viagem no espaço']
+  },
+  {
+    trailer:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover:'assets/images/guardios-da-galaxia.jpg',
+    name:'Guardiões da Galaxia 2',
+    relevance:80,
+    age:0,
+    parts:2,
+    categories:['Épico', 'Filme de fantasia','Viagem no espaço']
+  },
+  {
+    trailer:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover:'assets/images/guardios-da-galaxia.jpg',
+    name:'Guardiões da Galaxia 3',
+    relevance:55,
+    age:18,
+    parts:2,
+    categories:['Épico', 'Filme de fantasia','Viagem no espaço']
+  },
+  {
+    trailer:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover:'assets/images/guardios-da-galaxia.jpg',
+    name:'Guardiões da Galaxia 3',
+    relevance:55,
+    age:18,
+    parts:2,
+    categories:['Épico', 'Filme de fantasia','Viagem no espaço']
+  }
+];
+
 }
